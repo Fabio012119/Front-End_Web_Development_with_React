f530b70e542108a4d27b4ef1d6930bc72c4215c8
Fabio Saraseli <fabiooosara@gmail.com>
2022-07-26 05:20:46 -0700

    React Redux Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -56,7 +56,7 @@ const DishWithId = ({match}) => {
         <Header/>
         <Switch>
           <Route path="/home" component={HomePage}/>
-          <Route exact path="/menu" component={() =><Menu dishes={this.props.dishes} />}/>
+          <Route exact path="/menu" component={() =><Menu dishes={this.props.dishes}/>}/>
           <Route path="/menu/:dishId" component={DishWithId}/>
           <Route exact path="/contactus" component={Contact}/>
           <Route exact path="/aboutus" component={ () => <About leaders={this.props.leaders} /> } />
