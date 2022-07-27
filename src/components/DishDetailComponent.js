d75ed61c521a2ea95b7858ab5bda958dcbabe9b0
Fabio Saraseli <fabiooosara@gmail.com>
2022-07-27 04:30:27 -0700

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,12 +3,14 @@ import { Card,CardImg,CardText,CardBody,CardTitle,Breadcrumb,BreadcrumbItem,} fr
 import {Link} from 'react-router-dom';
 import CommentForm from "./CommentFormComponent";
 import{Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+
 
   function RenderDish({dish}){
     return (
     <div className="col-12 col-md-5 m-1">
       <Card>
-          <CardImg top src={dish.image} alt={dish.name}/>
+          <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
           <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>
